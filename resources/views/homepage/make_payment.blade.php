ac0dbbcf09271b0fc002388c925229241c9582e4
Adekoya Adebayo <adekoya.adebayojubril@gmail.com>
2021-11-18 12:40:19 +0100

    pay from homepage

--- resources/views/homepage/make_payment.blade.php
+++ resources/views/homepage/make_payment.blade.php
@@ -100,7 +100,7 @@
 
                                         <div class="col-lg-4">
                                             <input type="hidden" name="callback_url" id="callback_url" value="https://imsu.edu.ng/api/save_application_fee/{{base64_encode($fee->id)}}">
-                                            <input type="hidden" name="callback_url" id="callback_url_interswitch" value="http://imsu.edu.ng/api/save_direct_interswitch">
+                                            <input type="hidden" name="callback_url" id="callback_url_interswitch" value="https://imsu.edu.ng/api/save_direct_interswitch">
                                             <!-- <input type="hidden" name="email" id="email" value="{{$std->email}}">
                                             <input type="hidden" name="phone" id="phone" value="{{$std->phone_number}}">
                                             <input type="hidden" name="first_name" id="first_name" value="{{$std->full_name}}">
