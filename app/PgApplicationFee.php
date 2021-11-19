a6a8b4f234bbac496bf543bc48a992901dd031a4
Adekoya Adebayo <adekoya.adebayojubril@gmail.com>
2021-11-19 16:02:16 +0100

    forget password for applicant

--- app/PgApplicationFee.php
+++ app/PgApplicationFee.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class PgApplicationFee extends Model
 {
     //
+    protected $fillable = ['amount', 'application_number', 'name', 'phone', 'email', 'reference_id', 'payment_channel', 'status'];
 }
