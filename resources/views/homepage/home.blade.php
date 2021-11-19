a8561912321f49ca4002e5b167fa141ca8e75d9d
Adekoya Adebayo <adekoya.adebayojubril@gmail.com>
2021-11-19 11:06:09 +0100

    remita added to make-payment page

--- resources/views/homepage/home.blade.php
+++ resources/views/homepage/home.blade.php
@@ -1132,14 +1132,14 @@
          </div>
          <div class="modal-body">
             <div class="text-center">
-               <a href="/article/26/20212022-post-utme-and-direct-entry-screening-exercise">
-                  <img src="homepage/upload/special_annoucement.jpg" class="img-responsive" alt="">
+               <a href="/article/25/course-registration-and-examination-card">
+                  <img src="homepage/upload/special_annoucement3.jpg" class="img-responsive" alt="">
                </a>
                <!-- <p>Registration of students on the portal has commenced Click here to register
                   </p> -->
                   <div class="row">
-                     <a href="/article/26/20212022-post-utme-and-direct-entry-screening-exercise"><span class="btn btn-primary">Read More</span></a>
-                     <a href="/admission-portal"><span class="btn btn-primary">Register</span></a>
+                     <a href="/article/25/course-registration-and-examination-card"><span class="btn btn-primary">Read More</span></a>
+                     <!-- <a href="/admission-portal"><span class="btn btn-primary">Register</span></a> -->
                      
                   </div>
             </div>
