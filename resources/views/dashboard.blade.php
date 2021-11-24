7970355e3fb5f0a6f18ad1ac66843f9ac1b1c94b
Adekoya Adebayo <adekoya.adebayojubril@gmail.com>
2021-11-24 15:01:41 +0100

    admin can see pg applicant

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -216,7 +216,7 @@
         <div class="col-sm-6 col-md-4">
             <div class="card card-stats card-round">
                 <div class="card-body">
-                    <a href="/admin/manage-admission">
+                    <a href="/admin/view-applicants">
                         <div class="row align-items-center">
                             <div class="col-icon">
                                 <div class="icon-big text-center icon-success bubble-shadow-small">
@@ -238,7 +238,7 @@
         <div class="col-sm-6 col-md-4">
             <div class="card card-stats card-round">
                 <div class="card-body">
-                    <a href="/admin/manage-admission">
+                    <a href="/admin/view-applicants">
                         <div class="row align-items-center">
                             <div class="col-icon">
                                 <div class="icon-big text-center icon-success bubble-shadow-small">
@@ -260,7 +260,7 @@
         <div class="col-sm-6 col-md-4">
             <div class="card card-stats card-round">
                 <div class="card-body">
-                    <a href="/admin/manage-admission">
+                    <a href="/admin/view-applicants">
                         <div class="row align-items-center">
                             <div class="col-icon">
                                 <div class="icon-big text-center icon-success bubble-shadow-small">
@@ -282,7 +282,7 @@
         <div class="col-sm-6 col-md-4">
             <div class="card card-stats card-round">
                 <div class="card-body">
-                    <a href="/admin/manage-admission">
+                    <a href="/admin/view-applicants">
                         <div class="row align-items-center">
                             <div class="col-icon">
                                 <div class="icon-big text-center icon-success bubble-shadow-small">
@@ -304,7 +304,7 @@
         <div class="col-sm-6 col-md-4">
             <div class="card card-stats card-round">
                 <div class="card-body">
-                    <a href="/admin/manage-admission">
+                    <a href="/admin/view-applicants">
                         <div class="row align-items-center">
                             <div class="col-icon">
                                 <div class="icon-big text-center icon-success bubble-shadow-small">
